2bbbd0d3b59a5e029fee0c96b18ff969fbe48f05
Hamed Haselimashhadi <hamedhm@ebi.ac.uk>
2019-07-27 20:40:10 +0000

    version 3.0.0

--- R/GridSearchModel.R
+++ R/GridSearchModel.R
@@ -1,107 +1,124 @@
-gridSearchModel = function(object            ,
-                           data              ,
-                           t                 ,
-                           m = mean(t)       ,
-                           l = 1             ,
-                           k = 1             ,
+gridSearchModel = function(object                                     ,
+                           data                                       ,
+                           t                                          ,
+                           m = mean(t)                                ,
+                           l = 1                                      ,
+                           k = 1                                      ,
                            weightFUN = function(x, ...) {
                              x
-                           }                   ,
-                           threshold = 10 ^ -18,
-                           check = 1           ,
-                           messages = TRUE     ,
-                           onlyOne  = FALSE    ,
+                           }                                          ,
+                           threshold = sqrt(.Machine$double.eps) * 10 ,
+                           check = 1                                  ,
+                           messages = TRUE                            ,
+                           onlyOne  = FALSE                           ,
+                           cdf                                        ,
+                           zeroCompensation = 0                       ,
+                           weightOrthreshold = 'weight'               ,
+                           direction         = c(1, 1)                ,
                            ...) {
-  lk = length(k)
-  ll = length(l)
-  m  = unique(m)
+  lk     = length(k)
+  ll     = length(l)
+  m      = unique(m)
   ## outputs
-  lmodel = lweight = list()
+  lmodel = lweight = orgWeight = list(NULL)
   rmat   = matrix(0, ncol = 4       , nrow = ll * lk)
-  colnames(rmat)          = c('Ind',
+  colnames(rmat)          = c('Index'        ,
                               'ObsInInterval',
-                              'k',
+                              'k'            ,
                               'l')
   counter = countProgress = 1
-  pb = txtProgressBar(
-    min = 0,
-    max = lk * ll,
-    style = 3,
+  pb      = txtProgressBar(
+    min   = 0       ,
+    max   = lk * ll ,
+    style = 3     ,
     width = 50
   )
-  for (lp in l) {
-    for (kp in k) {
+  for (kp in k) {
+    for (lp in l) {
       weight = expWeight(
-        t = t            ,
-        k = kp           ,
-        l = lp           ,
-        m = m            ,
-        plot = 0
+        t = t                               ,
+        k = kp                              ,
+        l = lp                              ,
+        m = m                               ,
+        plot = 0                            ,
+        cdf  = cdf                          ,
+        zeroCompensation = zeroCompensation ,
+        direction = direction               ,
+        progress = FALSE                    
       )
-      
       wi      = checkWeightsN(
         w         = weight    ,
         threshold = threshold ,
         t         = t         ,
         check     =  check
       )
-      inn                 = wi$NoZeWe
+      if(weightOrthreshold == 'weight')
+        inn                 = sum(wi$w)
+      else
+        inn = wi$NoZeWe
+      
       newdata             = data[wi$wInd,]
       newdata$ModelWeight = wi$w
       slm = tryCatch(
         expr = {
           do.call('update',
                   list(
-                    object                        ,
-                    weights  = weightFUN (newdata$ModelWeight)                ,
-                    data     = droplevels(newdata),
+                    object                                     ,
+                    weights  = weightFUN (newdata$ModelWeight) ,
+                    data     = droplevels(newdata)             ,
                     ...
                   ))
         },
         error = function(err, messsages = messages) {
           if (messsages)
-            message('\n Error: ', err)
+            message0(' ~> Error: '  , err)
           slm = NULL
         } ,
         warning = function(war, messsages = messages) {
           if (messsages)
-            message('\n Warning: ', war)
+            message0(' ~> Warning: ', war)
           slm = NULL
         }
       )
-      # Progress bar
       setTxtProgressBar(pb = pb, value = countProgress)
       countProgress = countProgress + 1
       # pass or skip
       if (is.null(slm)) {
-        rmat = rmat[-nrow(rmat), ]
+        rmat = rmat[-nrow(rmat), , drop = FALSE]
         next
       }
       
-      lmodel[[counter]]   = slm
-      lweight[[counter]]  = wi$w
-      rmat  [counter, ]    = c(counter ,
-                               sum(inn),
-                               kp      ,
-                               lp)
-      counter           = counter  + 1
+      lmodel[[counter]]      = slm
+      lweight[[counter]]     = wi$w
+      orgWeight[[counter]]   = weight
+      rmat  [counter, ]      = c(counter  ,
+                                 inn      ,
+                                 kp       ,
+                                 lp)
+      counter                = counter  + 1
     }
   }
   close(pb)
-  cat('\n')
-  
-  
-  return(list(
-    output = as.data.frame(rmat),
-    models = if (onlyOne && length(lmodel) < 2) {
-      lmodel[[1]]
-    } else{
-      lmodel
-    },
-    weights = if (onlyOne && length(lweight) < 2) {
-      lweight[[1]]
-    } else{
-      lweight
-    }
-  ))
+
+  return(
+    list(
+      output = as.data.frame(rmat),
+      data   = data               ,
+      models = if (onlyOne     && length(lmodel) < 2) {
+        lmodel[[1]]
+      } else{
+        lmodel
+      },
+      weights = if (onlyOne    && length(lweight) < 2) {
+        lweight[[1]]
+      } else{
+        lweight
+      },
+      FullWeight = if (onlyOne && length(orgWeight) < 2) {
+        orgWeight[[1]]
+      } else{
+        orgWeight
+      }
+    )
+  )
 }
